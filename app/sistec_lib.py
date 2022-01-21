d988cd047eb4d3be442907a32acc7e598887b5fe
Joao Manoel <joaomanoel7@gmail.com>
2022-01-21 16:40:21 -0300

    atualização no help das colunas das planilhas do sistec no sistec_lib.py e colocando no log qual o número da tentativa de baixar os arquivos da turma no SISTEC ...

--- app/sistec_lib.py
+++ app/sistec_lib.py
@@ -101,17 +101,17 @@ def write_csv(file_csv, no_campus, co_campus, perfil, qtdPerfis, tipos, cookies,
     alunos_json = path.join(getenv('SISTEC_DOWNLOAD_APP', default=path.curdir), '_alunos.json')
 
     # COLUNAS DO ARQUIVO CSV
-    # 00: CO_ALUNO_IDENTIFICADO     # 01: CO_ALUNO                     # 02: NO_ALUNO                 # 03: NO_MAE_ALUNO       # 04: NO_NOME_SOCIAL
-    # 05: NU_CPF                    # 06: DS_EMAIL                     # 07: CO_PESSOA_FISICA_ALUNO   # 08: DS_SENHA           # 09: CO_MATRICULA
-    # 10: CO_CICLO_MATRICULA        # 11: CO_STATUS_CICLO_MATRICULA    # 12: CO_CURSO                 # 13: NU_CARGA_HORARIA   # 14: DT_DATA_INICIO
-    # 15: DT_DATA_FIM_PREVISTO      # 16: CO_UNIDADE_ENSINO            # 17: CO_PERIODO_CADASTRO      # 18: NO_CICLO_MATRICULA # 19: ST_ATIVO
-    # 20: CO_TIPO_OFERTA_CURSO      # 21: CO_TIPO_INSTITUICAO          # 22: CO_INSTITUICAO           # 23: CO_PORTFOLIO       # 24: CO_TIPO_NIVEL_OFERTA_CURSO
-    # 25: CO_TIPO_PROGRAMA_CURSO    # 26: ST_CARGA                     # 27: DT_DATA_FINALIZADO       # 28: NU_VAGAS_OFERTADAS # 29: NU_TOTAL_INSCRITOS
-    # 30: ST_ETEC                   # 31: CO_POLO                      # 32: UAB # 33: ST_PREVISTO    # 34: NU_VAGAS_PREVISTAS # 35: CO_CURSO_SUPERIOR_CORRELATO
-    # 36: NU_CARGA_HORARIA_ESTAGIO  # 37: NO_ARQUIVO                   # 38: NO_CAMINHO_ARQUIVO       # 39: ST_ESTAGIO         # 40: ST_EXPERIMENTAL
-    # 41: NO_STATUS_MATRICULA       # 42: CO_PESSOA_FISICA             # 43: NU_RG # 44: SG_SEXO      # 45: DT_NASCIMENTO      # 46: NO_PESSOA_FISICA
-    # 47: CO_PESSOA                 # 48: CO_CARGO                     # 49: DS_ORGAO_EXPEDIDOR       # 50: SG_UF_ORG_EXPED
-    # 51: DS_CARGO                  # 52: CO_UNIDADE_ENSINO_IMPORTACAO # 53: CO_MATRICULA_RESPONSAVEL # 54: NO_SOCIAL
+    # 00: CO_ALUNO_IDENTIFICADO       # 01: CO_ALUNO                     # 02: NO_ALUNO                     # 03: NO_MAE_ALUNO             # 04: NO_NOME_SOCIAL
+    # 05: NU_CPF                      # 06: DS_EMAIL                     # 07: CO_PESSOA_FISICA_ALUNO       # 08: DS_SENHA                 # 09: CO_MATRICULA
+    # 10: CO_CICLO_MATRICULA          # 11: CO_STATUS_CICLO_MATRICULA    # 12: CO_CURSO                     # 13: NU_CARGA_HORARIA         # 14: DT_DATA_INICIO
+    # 15: DT_DATA_FIM_PREVISTO        # 16: CO_UNIDADE_ENSINO            # 17: CO_PERIODO_CADASTRO          # 18: NO_CICLO_MATRICULA       # 19: ST_ATIVO
+    # 20: CO_TIPO_OFERTA_CURSO        # 21: CO_TIPO_INSTITUICAO          # 22: CO_INSTITUICAO               # 23: CO_PORTFOLIO             # 24: CO_TIPO_NIVEL_OFERTA_CURSO
+    # 25: CO_TIPO_PROGRAMA_CURSO      # 26: ST_CARGA                     # 27: DT_DATA_FINALIZADO           # 28: NU_VAGAS_OFERTADAS       # 29: NU_TOTAL_INSCRITOS
+    # 30: ST_ETEC                     # 31: CO_POLO                      # 32: UAB                          # 33: ST_PREVISTO              # 34: NU_VAGAS_PREVISTAS
+    # 35: CO_CURSO_SUPERIOR_CORRELATO # 36: NU_CARGA_HORARIA_ESTAGIO     # 37: NO_ARQUIVO                   # 38: NO_CAMINHO_ARQUIVO       # 39: ST_ESTAGIO
+    # 40: ST_EXPERIMENTAL             # 41: NO_STATUS_MATRICULA          # 42: CO_PESSOA_FISICA             # 43: NU_RG                    # 44: SG_SEXO
+    # 45: DT_NASCIMENTO               # 46: NO_PESSOA_FISICA             # 47: CO_PESSOA                    # 48: CO_CARGO                 # 49: DS_ORGAO_EXPEDIDOR
+    # 50: SG_UF_ORG_EXPED             # 51: DS_CARGO                     # 52: CO_UNIDADE_ENSINO_IMPORTACAO # 53: CO_MATRICULA_RESPONSAVEL # 54: NO_SOCIAL
     # USAREMOS AS SEGUINTES COLUNAS:
     # 02; 05; 12; 14; 15; 18; 20; 31; 41
 
@@ -370,6 +370,13 @@ def write_csv(file_csv, no_campus, co_campus, perfil, qtdPerfis, tipos, cookies,
                 file_json_r = open(turmas_json, mode='rb+')
                 turmas_data = json.load(file_json_r)
                 file_json_r.close()
+                if level_debug[0]:
+                    with open(file_log_path, 'a') as file_log:
+                        file_log.write(
+                            u'%s %d tentativa(s): ...\n' %
+                            (strftime('[%Y-%m-%d %H:%M:%S]'), count + 1))
+                        file_log.flush()
+                        fsync(file_log.fileno())
                 if level_debug[1]:
                     with open(file_log_path, 'a') as file_log:
                         file_log.write(u'%s turmas_data:\t%s\n' % (
